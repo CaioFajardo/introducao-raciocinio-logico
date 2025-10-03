dc4b4e931396d7d4437f9a37d4309d88bc94f337
CaioFajardo <caiocarvalhofajardo@gmail.com>
2025-10-02 22:14:31 -0300

    Alterações realizadas em sala de aula

--- connectors/not.js
+++ connectors/not.js
@@ -2,3 +2,18 @@
 
 // Este conector INVERTE o valor lógico da condição.
 // Se a condição é verdadeira, o '!' a torna falsa, e vice-versa.
+
+// Eu posso votar caso:
+// - Eu seja homem OU mulher
+// - Eu NÃO seja menor de idade
+// - Tenha titulo de eleitor
+
+let idade = 18
+let possuiCNH = true
+let nacionalidade = "brasileiro"
+
+if (!(idade >= 18 && possuiCNH == true && nacionalidade == "brasileiro")) {
+    console.log("Você não pode dirigir")
+} else {
+    console.log("Você pode dirigir")
+}
---
+++ explicacoes/index.js
@@ -0,0 +1,13 @@
+// Sinais comparativos
+// > maior que
+// < menor que
+// >= maior ou igual a
+// <= menor ou igual a
+// == igual a
+// === Igual com valização de tipagem
+// != diferente de
+// !== diferente com valização de tipagem
+
+// = atribuição
+// == Igualdade
+// === Igualdade levando em consideração o tipo 2 === "2" -> false
